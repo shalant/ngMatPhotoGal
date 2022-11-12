4030aa26d0bc9e5674b22fa2e895944489856f93
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-11 20:56:41 -0600

    finally getting somewhere

--- attempt2/src/app/portfolio/portfolio.component.ts
+++ attempt2/src/app/portfolio/portfolio.component.ts
@@ -1,22 +1,45 @@
-import {Component} from '@angular/core';
-
-const HEROES = [
-  {id: 1, name:'Superman'},
-  {id: 2, name:'Batman'},
-  {id: 5, name:'BatGirl'},
-  {id: 3, name:'Robin'},
-  {id: 4, name:'Flash'}
-];
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'heroes',
-  template: 'portfolio.component.html',
-  styleUrls: ['portfolio.component.css'],
+  selector: 'app-root',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
-cards: any;
-}
+  title = 'angular-project';
+  friendslist = [
+    {
+      name: 'Nishant',
+      age: 25
+    },
+    {
+      name: 'Shailesh',
+      age: 45
+    },
+    {
+      name: 'Abhishek',
+      age: 36
+    },
+    {
+      name: 'Akshay',
+      age: 65
+    },
+    {
+      name: 'Ashish',
+      age: 12
+    },
+    {
+      name: 'Uday',
+      age: 31
+    },
+    {
+      name: 'Mayank',
+      age: 45
+    },
+    {
+      name: 'Raju',
+      age: 74
+    },
+  ]
 
-export class Heroes {
-  heroes = HEROES;
 }
